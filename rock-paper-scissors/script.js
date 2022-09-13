11934357bf1dc90641db1b09a25dd3406d7d2fbe
Juanma Romero <pro@juanmaroni.me>
2022-09-13 12:56:03 +0200

    Play a round function and test

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -11,4 +11,41 @@ getComputerChoice = () => {
     }
 }
 
-alert(getComputerChoice());
+playRound = (playerSelection, computerSelection) => {
+    // Convert to lower case
+    let playerSel = playerSelection.toLowerCase();
+    let computerSel = computerSelection.toLowerCase();
+
+    if (playerSel === 'rock') {
+        if (computerSel === 'scissors') {
+            return 'You Win! Rock beats Scissors';
+        } else if (computerSel === 'paper') {
+            return 'You Lose! Paper beats Rock';
+        } else {
+            return 'That\'s a tie!';
+        }
+    } else if (playerSel === 'paper') {
+        if (computerSel === 'rock') {
+            return 'You Win! Paper beats Rock';
+        } else if (computerSel === 'scissors') {
+            return 'You Lose! Scissors beats Paper';
+        } else {
+            return 'That\'s a tie!';
+        }
+    } else if (playerSel === 'scissors') {
+        if (computerSel === 'paper') {
+            return 'You Win! Scissors beats Paper';
+        } else if (computerSel === 'rock') {
+            return 'You Lose! Rock beats Scissors';
+        } else {
+            return 'That\'s a tie!';
+        }
+    } else {
+        return 'An error, WTF!';
+    }
+}
+
+const playerSelection = 'papeR';
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
