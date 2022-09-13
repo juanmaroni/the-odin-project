f10b44addedcbb433975428288e4852ae30a0df5
Juanma Romero <pro@juanmaroni.me>
2022-09-13 12:29:54 +0200

    Computer plays and shows alert

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,1 +1,14 @@
+getComputerChoice = () => {
+    let n = Math.floor(Math.random() * 3);
+    console.log('Got number ' + n);
 
+    if (n == 0) {
+        return 'Rock';
+    } else if (n == 1) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
+
+alert(getComputerChoice());
