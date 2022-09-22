7f4f9b6daba4f001ec7e843690a9b8e05690fec3
Juanma Romero <pro@juanmaroni.me>
2022-09-22 18:05:15 +0200

    Random colors and reordering JS code

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,6 +1,19 @@
 // Select grid
 const grid = document.querySelector('#grid');
 
+// Default grid on page load
+generateGrid(16);
+
+// Set grid size
+const btnSetGrid = document.querySelector('#btn-set-size');
+btnSetGrid.addEventListener('click', () => {
+    document.querySelector('#grid').replaceChildren();
+    let size = Number(prompt('Set size: min 1, max 30, default 16'));
+    size = size < 1 || size > 30 ? 16 : size;
+    
+    generateGrid(size);
+});
+
 // Generate a grid with a given size
 function generateGrid(size) {
     for (i = 0; i < size; i++) {
@@ -21,21 +34,17 @@ function generateGrid(size) {
     // Add event to paint when a square is clicked
     const lstSquares = document.querySelectorAll('.square');
     lstSquares.forEach((square) => {
-    square.addEventListener('click', () => {
-        square.classList.toggle('square-painted');
-    });
-})
+        square.addEventListener('click', () => {
+            // Paint a random color
+            square.style.backgroundColor = generateRgbColor();
+        });
+    })
 }
 
-// Default grid on page load
-generateGrid(16);
+function generateRgbColor() {
+    return `rgb(${getRandomColor()},${getRandomColor()},${getRandomColor()})`;
+}
 
-// Set grid size
-const btnSetGrid = document.querySelector('#btn-set-size');
-btnSetGrid.addEventListener('click', () => {
-    document.querySelector('#grid').replaceChildren();
-    let size = Number(prompt('Set size: min 1, max 30, default 16'));
-    size = size < 1 || size > 30 ? 16: size;
-    
-    generateGrid(size);
-});
+function getRandomColor() {
+    return Math.floor(Math.random() * 255);
+}
