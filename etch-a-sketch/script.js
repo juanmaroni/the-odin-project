9c0957977035952135a23894be40a00a88d1a636
Juanma Romero <pro@juanmaroni.me>
2022-09-22 11:42:30 +0200

    16x16 numbered green grid

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,5 +1,12 @@
-const square = document.createElement('div');
-square.classList.toggle('square');
-
+// Select grid
 const grid = document.querySelector('#grid');
-grid.appendChild(square);
+
+// Append squares
+for (i = 0; i < 16; i++) {
+    for (j = 0; j < 16; j++) {
+        const square = document.createElement('div');
+        square.classList.toggle('square');
+        square.textContent = i;
+        grid.appendChild(square);
+    }
+}
