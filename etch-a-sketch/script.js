3bbd6d383ee30184e9181c5bc5e5c1fdfa0d9b67
Juanma Romero <pro@juanmaroni.me>
2022-09-22 17:08:24 +0200

    Select grid size

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,21 +1,41 @@
 // Select grid
 const grid = document.querySelector('#grid');
 
-// Append squares
-for (i = 0; i < 16; i++) {
-    for (j = 0; j < 16; j++) {
-        const square = document.createElement('div');
-        square.classList.toggle('square');
-        grid.appendChild(square);
+// Generate a grid with a given size
+function generateGrid(size) {
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            const square = document.createElement('div');
+            square.classList.toggle('square');
+            grid.appendChild(square);
+        }
     }
-}
 
-// Get squares
-const lstSquares = document.querySelectorAll('.square');
+    // Change grid width based on size
+    grid.style.width = '' + size * 40;
+    // Distribute the squares
+    let gridTemplateRepeat = 'repeat(' + size + ', 40px)';
+    grid.style.gridTemplateColumns = gridTemplateRepeat;
+    grid.style.gridTemplateRows = gridTemplateRepeat;
 
-// Painting while clicking
-lstSquares.forEach((square) => {
+    // Add event to paint when a square is clicked
+    const lstSquares = document.querySelectorAll('.square');
+    lstSquares.forEach((square) => {
     square.addEventListener('click', () => {
         square.classList.toggle('square-painted');
     });
 })
+}
+
+// Default grid on page load
+generateGrid(16);
+
+// Set grid size
+const btnSetGrid = document.querySelector('#btn-set-size');
+btnSetGrid.addEventListener('click', () => {
+    document.querySelector('#grid').replaceChildren();
+    let size = Number(prompt('Set size: min 1, max 30, default 16'));
+    size = size < 1 || size > 30 ? 16: size;
+    
+    generateGrid(size);
+});
