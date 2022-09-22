bfd71aea9ee578579299e4d44310e8a4ab11c222
Juanma Romero <pro@juanmaroni.me>
2022-09-22 13:26:18 +0200

    Clickpainting white over black

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -6,7 +6,16 @@ for (i = 0; i < 16; i++) {
     for (j = 0; j < 16; j++) {
         const square = document.createElement('div');
         square.classList.toggle('square');
-        square.textContent = i;
         grid.appendChild(square);
     }
 }
+
+// Get squares
+const lstSquares = document.querySelectorAll('.square');
+
+// Painting while clicking
+lstSquares.forEach((square) => {
+    square.addEventListener('click', () => {
+        square.classList.toggle('square-painted');
+    });
+})
