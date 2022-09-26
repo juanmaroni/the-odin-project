4ac01b829d85648cc71298f04f733c3a85bac73f
Juanma Romero <pro@juanmaroni.me>
2022-09-26 12:56:09 +0200

    Clear button

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,11 @@
+const output = document.querySelector('#output');
+
+// Clear button
+const btnClear = document.querySelector('#clear');
+btnClear.addEventListener('click', () => {
+    clear();
+});
+
 function add(a, b) {
     return a + b;
 }
@@ -12,4 +20,8 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function clear() {
+    output.textContent = '0';
 }
